ab5a7c9159e4c80907d1410ae4aeaf6ae8e955e6
陈既谋 <1625645320@qq.com>
2020-06-26 00:37:26 +0800

    deal car plate image 0037

--- identity/deal_plate_image.py
+++ identity/deal_plate_image.py
@@ -3,6 +3,8 @@
 # @Author  :  Jimou Chen
 """
 import cv2
+import numpy as np
+import tensorflow as tf
 
 car_plate_img = cv2.imread('./car_plate_photo/1.jpg', cv2.IMREAD_GRAYSCALE)
 print(car_plate_img)
