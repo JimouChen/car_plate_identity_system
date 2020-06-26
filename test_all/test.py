7b30df2a3ff04b466d041b2fa0ac9eb609ff3472
陈既谋 <1625645320@qq.com>
2020-06-26 11:19:43 +0800

    test tesseract

--- test_all/test.py
+++ test_all/test.py
@@ -2,3 +2,14 @@
 # @Time    :  2020/6/26
 # @Author  :  Jimou Chen
 """
+import pytesseract
+import cv2
+
+# 1.引入Tesseract程序
+pytesseract.pytesseract.tesseract_cmd = r'D:\Appication\PyCharm\TesseractOCR\tesseract.exe'
+image = cv2.imread('D:/testocr.png')
+print(image)
+
+# 2.识别图片文字
+code = pytesseract.image_to_string(image)
+print(code)
